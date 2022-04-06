675492b5788412153c3610e08dae6543c792a734
John Brown <johnbrownsinbox@gmail.com>
2022-04-06 09:59:34 -0400

    commit tree a4985aaa77493c069edf4899cbcee61b12a3e265

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,7 @@
-# Write your code here.
+def badge_maker inputName
+"Hello, my name is #{inputName}." 
+end
+
+def batch_badge_creator nameArray
+badge_message_array=nameArray.collect{|eachName|badge_maker(eachName)}    
+end
